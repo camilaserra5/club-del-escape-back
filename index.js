8494b265e57adc95081e6694bf6b2b060d35b41e
camilaserra5 <camilaserra5@gmail.com>
2022-09-22 22:58:08 -0300

    test

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const express = require("express");
+
+
+const express = require('express');
 const request = require('request');
 
 const app = express();
 
-app.get("/", (req, res) => {
-    request({
-        uri: 'https://api.bookeo.com/v2/availability/slots?apiKey=AU63AAEP4RCTEHAXM9HXJ41558697WMR1777F47D4D4&secretKey=IPEwY87K3Eoskq4c2EiDQ0jP4RwM2P5A&startTime=2022-09-23T09:00:00-03:00&endTime=2022-09-24T03:00:00-03:00&itemsPerPage=300',
-
-    }).pipe(res);
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
 });
 
-app.listen(5000, () => {
-    console.log("Running on port 5000.");
+app.get('/slots', (req, res) => {
+    request(
+        { url: 'https://api.bookeo.com/v2/availability/slots?apiKey=AU63AAEP4RCTEHAXM9HXJ41558697WMR1777F47D4D4&secretKey=IPEwY87K3Eoskq4c2EiDQ0jP4RwM2P5A&startTime=2022-09-23T09:00:00-03:00&endTime=2022-09-24T03:00:00-03:00&itemsPerPage=300'},
+        (error, response, body) => {
+            if (error || response.statusCode !== 200) {
+                return res.status(500).json({ type: 'error', message: err.message });
+            }
+
+            res.json(JSON.parse(body));
+        }
+    )
 });
 
-// Export the Express API
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
