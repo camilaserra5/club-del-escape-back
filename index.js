f95fef8b468fddef4d11747149b42f70b08abdfc
camilaserra5 <camilaserra5@gmail.com>
2022-09-22 23:22:40 -0300

    test

--- index.js
+++ index.js
@@ -11,11 +11,16 @@ app.use((req, res, next) => {
 });
 
 app.get('/slots', (req, res) => {
+    let dateFrom = req.query.dateFrom; //2022-09-23
+    let dateTo = req.query.dateTo; //2022-09-24
+    let urlll = 'https://api.bookeo.com/v2/availability/slots?apiKey=AU63AAEP4RCTEHAXM9HXJ41558697WMR1777F47D4D4&secretKey=IPEwY87K3Eoskq4c2EiDQ0jP4RwM2P5A&startTime=' + dateFrom + 'T09:00:00-03:00&endTime=' + dateTo + 'T03:00:00-03:00&itemsPerPage=300'
+
+    console.log(urlll)
     request(
-        { url: 'https://api.bookeo.com/v2/availability/slots?apiKey=AU63AAEP4RCTEHAXM9HXJ41558697WMR1777F47D4D4&secretKey=IPEwY87K3Eoskq4c2EiDQ0jP4RwM2P5A&startTime=2022-09-23T09:00:00-03:00&endTime=2022-09-24T03:00:00-03:00&itemsPerPage=300'},
+        { url: urlll},
         (error, response, body) => {
             if (error || response.statusCode !== 200) {
-                return res.status(500).json({ type: 'error', message: err.message });
+                return res.status(500).json({ type: 'error', message: error });
             }
 
             res.json(JSON.parse(body));
